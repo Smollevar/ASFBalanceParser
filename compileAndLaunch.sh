a52927e87c6146a5ce5bfdd61f434c4449f5ae74
Dmitrii Kravtsov <e4ahl1bhyr@gmail.com>
2025-03-24 12:03:29 +0300

    add script and delete comm

---
+++ compileAndLaunch.sh
@@ -0,0 +1,2 @@
+#!/bin/bash 
+gcc parser.c -o parserExec && ./parserExec > withBalance.txt
--- parser.c
+++ parser.c
@@ -16,28 +16,16 @@ int main(int argc, char *argv[])
             char *doubleDotPointer = NULL;
             doubleDotPointer = strchr(currentLine, ':');
             doubleDotPointer += 2;
-            // currentLine += 3;
             char *endptr = " ";
 
             long num = strtol(doubleDotPointer, &endptr, 10);
             if (num > 3000) {
                 printf("%s", line);
             }
-            
-            // printf ("digits is: %ld\n", num);
-            // printf("19 line is: %c\n", *doubleDotPointer);
-            // printf("%s", currentLine);
         }
 
         fclose(file);
 
-/*
-            1) Find pointer on symbol.
-            2) Shift him on 2 forward, that he will point on first digit.
-            3) Whether parse or collect digit.
-            4) Decide whether print the digit if it above than 3.000 value.
-            5) Iterate through all lines in file.
-*/
     }
     else
     {
